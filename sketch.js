32793245d34b51884d54d01a362fa66b8eb05d2b
YsaWV <YsaWV@users.noreply.github.com>
2021-07-14 12:21:22 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  let canvas = createCanvas(300, 300);
+  canvas.parent('sketch-container');
+  background(220);
+}
+
+function draw() {
+  
+  rect(mouseX, mouseY,40,40);
+  
+  let xLabel = document.getElementById('x-label');
+  xLabel.innerText = 'X: ' + mouseX;
+
+  let yLabel = document.getElementById('y-label');
+  yLabel.innerText = 'Y: ' + mouseY;
+}
